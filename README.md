5f51ffcc9fa28e6695d879cc048947aef1cf2b17
wangxuehao <wangxuehao@qiyi.com>
2018-03-27 14:17:24 +0800

    优化gulp文件

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # crossPageCommunicate
-跨页面通讯（不存在跨域限制）
+  跨页面通讯（不存在跨域限制）
+
+host:
+  127.0.0.1 iframe.iqiyi.com 
+  127.0.0.1 main.com
+  127.0.0.1 sub.com 
+启动demo
+  1. git clone https://github.com/wangxuehao/crossPageCommunicate.git
+  2. npm install
+  3. gulp dev
+
--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,30 @@
 var gulp = require('gulp');
 var gulpSequence = require('gulp-sequence');
 var gulpConnect = require("gulp-connect");
+var gulpOpen = require("gulp-open");
+var gulpConfig = require("./gulp.config.json");
+var portNum = gulpConfig.dev.port;
+
+//task：启动server
 gulp.task("server", function(){
   return gulpConnect.server({
-    port: "9023",
+    port: portNum,
     debug: true
   });
 });
-gulp.task("dev", gulpSequence('server'));
+
+//task：打开测试页
+gulp.task("openBrowser",function(){
+  var pageAUrl = gulpConfig.dev.pageAUrl;
+  var pageBUrl = gulpConfig.dev.pageBUrl;
+  return gulp.src('')
+    .pipe(gulpOpen({
+      uri:pageAUrl
+    }))
+    .pipe(gulpOpen({
+      uri:pageBUrl
+    }));
+});
+
+
+gulp.task("dev", gulpSequence(['server','openBrowser']));
