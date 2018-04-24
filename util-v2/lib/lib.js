688db5c5163cc05d1794907dc6bd48c3c64ff5d3
wangxuehao <wangxuehao@qiyi.com>
2018-04-24 17:13:28 +0800

    util第二版

--- util-v2/lib/lib.js
+++ util-v2/lib/lib.js
@@ -41,17 +41,17 @@ function on(type,listener){
       /**
        * arguments[0]要在window.event之前
        * IE10下，触发storage事件时
-       * arguments[0]和window.event都会有值
+       * arguments[0]和window.event都能取到event对象
        * 但是，window.event中取不到key和newValue等属性
        */
       var event = arguments[0] || window.event;
-      if(event.key == type && event.newValue !== ''){
-        //接收到事件后，清除该条存储数据
-        setLocalStorage(type,'');
+      if(event.key == type && event.newValue !== ""){
+        //接收到事件后，清除该条数据
+        localStorage.setItem(type,"");
         var eventData = event.newValue;
-        //newValue为util_empty_data时，表示传递值为空
-        if(event.newValue == 'util_empty_data'){
-          eventData = '';
+        try{
+          eventData = JSON.parse(eventData);
+        }catch(err){
         }
         listener({
           type:event.key,
@@ -68,21 +68,19 @@ function on(type,listener){
   window.addEventListener('storage',realListener);
 }
 
-function fire(event){
-  var eventData = event.data;
-  if( event.data == ''){
-    eventData = 'util_empty_data';
+function fire(type,data){
+  var eventData = data;
+  if(data === undefined || data === null ){
+    eventData = "";
   }
-  setLocalStorage(event.type,eventData);
-}
-
-function setLocalStorage(type,data){
-  localStorage.setItem(type, data);
+  eventData = JSON.stringify(eventData);
+  localStorage.setItem(type,eventData);
   //非IE浏览器，需要手动触发当前页的storage事件
   if(!isIE()){
-    noticeCurrentPage(type,data);
+    noticeCurrentPage(type,eventData);
   }
 }
+
 /**
  * 在当前页触发storage事件
  * https://developer.mozilla.org/en-US/docs/Web/API/StorageEvent
